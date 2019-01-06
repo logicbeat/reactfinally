5d06db5bfcff68e7700505aa5af3ac7e326c2fe6
Ashish Gupta <codegroove@outlook.com>
2019-01-06 20:42:17 +0530

    Code commited with a sample application with usage of Radium for inline styles
    Signed-off-by: Ashish Gupta <codegroove@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Radium, {StyleRoot} from 'radium';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { id: '12',name: "Max", age: 28 },
+      { id: 'ww21',name: "Ashu", age: 26 },
+      { id: 're1',name: "Abhi", age: 24 }
+    ],
+    showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log("Radha Soami Ji");
+    // DON'T DO THIS : this.state.persons[0].name = "Ashish Gupta";
+    this.setState({
+      persons: [
+        { id: '12',name: newName, age: 28 },
+        { id: 'ww21',name: "Ashu", age: 26 },
+        { id: 're1',name: "Abhi", age: 30 }
+      ]
+    })
+  }
+
+  nameChangeHandler = (event, id) => {
+    // console.log("Radha Soami Ji");
+    // DON'T DO THIS : this.state.persons[0].name = "Ashish Gupta";
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // const person = {
+    //   ...this.state.persons[personIndex]
+    // }
+    const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
+
+  togglePersonHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons : persons});
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color : 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor : 'lightgreen',
+        color : 'black'
+      }
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (<div>
+        {this.state.persons.map((person, index) => {
+          return (
+          <Person click={this.deletePersonHandler.bind(this, index)}
+           name={person.name}
+           age={person.age}
+           key={person.id}
+           changed={(event) => this.nameChangeHandler(event, person.id)}/>
+           )
+        })}
+      </div>)
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor : 'salmon',
+        color : 'black'
+      }
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
     return (
+      <StyleRoot>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Radha Soami Ji</h1>
+        <p className={classes.join(' ')}>This is really working!!!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
+        {persons}
       </div>
+      </StyleRoot>
     );
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Radha Soami Ji'));
   }
 }
 
-export default App;
+export default Radium(App);
---
+++ src/Person/Person.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Person.css";
+import Radium from 'radium';
+
+const person = (props) => {
+    let style = {
+        '@media (min-width:500px)' : {
+            width: '300px'
+        }
+    }
+    return (
+        <div className="Person" style={style}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}></input>
+        </div>
+    )
+}
+
+export default Radium(person);
